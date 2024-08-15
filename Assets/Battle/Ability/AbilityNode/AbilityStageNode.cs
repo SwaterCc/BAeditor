089c40124601db1d190789278eb963e2fcbb8c79
shirui <1245332977@qq.com>
2024-08-16 00:02:23 +0800

    1

--- Assets/Battle/Ability/AbilityNode/AbilityStageNode.cs
+++ Assets/Battle/Ability/AbilityNode/AbilityStageNode.cs
@@ -1,10 +1,16 @@
+using Battle.Def;
+
 namespace Battle
 {
     public partial class Ability
     {
-        private class AbilityStageNode
+        private class AbilityStageNode : AbilityNode
         {
-        
+            public AbilityStageNode(AbilityExecutor executor, AbilityNodeData data) : base(executor, data) { }
+            public override void DoJob()
+            {
+                
+            }
         }   
     }
 }
