9ac9572204ab4f0d652a6f4f9750e9d27039dd6c
shirui <rui.shi@yo-star.com>
2024-08-27 18:44:00 +0800

    1

--- Assets/Battle/Actor/ActorLogic/HitBoxLogic.cs
+++ Assets/Battle/Actor/ActorLogic/HitBoxLogic.cs
@@ -1,76 +1,89 @@
-﻿using System.Collections.Generic;
-using Battle.Damage;
-using Battle.GamePlay;
-using Battle.Tools;
-using BattleAbility;
+﻿using Hono.Scripts.Battle.Tools;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Battle
-{
-    public class HitBoxLogic : ActorLogic
-    {
-        private HitBoxConfig _hitBoxConfig;
+namespace Hono.Scripts.Battle {
+	public class HitBoxLogic : ActorLogic {
+		private HitBoxData _hitBoxConfig;
+		private float _duration;
+		private float _effectDuration;
+		private float _intervalDuration;
+		private bool _isInEffect;
+		private int _count;
+		private Dictionary<BeHurtComp, int> _hitCountDict = new();
 
-        private float _duration;
+		public HitBoxLogic(int configId) : base(configId) { }
 
-        private float _intervalDuration;
+		protected override void onInit() {
+			
+			_duration = 0;
+			_intervalDuration = _hitBoxConfig.Interval;
+			_count = 0;
+			_isInEffect = false;
+		}
 
-        private int _count;
-
-
-        public HitBoxLogic(int configId) : base(configId) { }
-
-        protected override void onInit()
-        {
-            _duration = 0;
-            _intervalDuration = 0;
-            _count = 0;
-        }
-
-        protected override void registerChildComp() { }
+		protected override void registerChildComp() { }
 
 
-        private void hit()
-        {
-            List<int> targets = null;
-            switch (_hitBoxConfig.HitType)
-            {
-                case EHitType.Aoe:
-                    targets = CommonUtility.CheckAABB(_hitBoxConfig.AoeData, Vector3.one);
-                    break;
-                case EHitType.LockTarget:
-                    break;
-            }
+		private void hit() {
+			List<int> targets = null;
+			switch (_hitBoxConfig.HitType) {
+				case EHitType.Aoe:
+					targets = CommonUtility.CheckAABB(_hitBoxConfig.AoeData, Vector3.one);
+					break;
+				case EHitType.LockTarget:
+					break;
+			}
 
-            if (targets == null) return;
+			if (targets == null) return;
 
-            var damageInfo = new DamageInfo();
-            var source = this.GetSimpleAttr<int>(EAttributeType.Source).GetValue();
-            var attacker = ActorManager.Instance.GetActor(source).ActorLogic;
-            foreach (var target in targets)
-            {
-                var targetLogic = ActorManager.Instance.GetActor(target).ActorLogic;
-                var res = DamageLuaInterface.GetDamageResults(attacker, targetLogic, damageInfo);
-                targetLogic.GetComponent<BeHurtComp>().OnBeHurt(res);
-            }
-        }
+			var damageInfo = new DamageInfo();
+			var source = this.GetSimpleAttr<int>(EAttributeType.Source).GetValue();
+			var attacker = ActorManager.Instance.GetActor(source).ActorLogic;
+			foreach (var target in targets) {
+				var targetLogic = ActorManager.Instance.GetActor(target).ActorLogic;
+				var behitComp = targetLogic.GetComponent<BeHurtComp>();
+				if (_hitCountDict.TryGetValue(behitComp,out var count)) {
+					if (count >= _hitBoxConfig.ValidCount) {
+						continue;
+					}
+				}
+				else {
+					_hitCountDict.Add(behitComp, 1);
+				}
+				var res = DamageLuaInterface.GetDamageResults(attacker, targetLogic, damageInfo);
+				behitComp.OnBeHurt(res);
+			}
+		}
 
-        protected override void onTick(float dt)
-        {
-            //有效时间
-            if (_duration >= _hitBoxConfig.DelayTime && _duration <= _hitBoxConfig.DelayTime + _hitBoxConfig.Duration)
-            {
-                if (_intervalDuration >= _hitBoxConfig.Interval)
-                {
-                    _intervalDuration = 0;
-                    hit();
-                }
-            }
+		protected override void onTick(float dt) {
+			//有效时间
+			if (_duration >= _hitBoxConfig.DelayTime && _duration <= _hitBoxConfig.DelayTime + _hitBoxConfig.Duration) {
+				if (_intervalDuration >= _hitBoxConfig.Interval) {
+					_intervalDuration = 0;
+					_effectDuration = 0;
+					++_count;
+					_isInEffect = true;
+				}
 
-            _intervalDuration += dt;
-            _duration += dt;
+				if (_isInEffect) {
+					if (_effectDuration <= _hitBoxConfig.EffectTime && _count <= _hitBoxConfig.ValidCount) {
+						_effectDuration += dt;
+						hit();
+					}
+					else {
+						_isInEffect = false;
+					}
+				}
+				
+				if (!_isInEffect) {
+					_intervalDuration += dt;
+				}
+			}
+			
+			_duration += dt;
 
-            if (_duration >= _hitBoxConfig.Duration + _hitBoxConfig.DelayTime) { }
-        }
-    }
+			if (_duration >= _hitBoxConfig.Duration + _hitBoxConfig.DelayTime) { }
+		}
+	}
 }
